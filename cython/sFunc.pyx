aa3a268d2b5569e13e094ccd01a62021924fb81e
Patrick Henrici <patrickhenrici@gmx.de>
2014-05-13 11:58:24 +0200

    cython types for ../sFunc.py

--- cython/sFunc.pyx
+++ cython/sFunc.pyx
@@ -10,102 +10,31 @@ from numpy import linalg
 # Opens an image with the specified path
 # Returns a numpy-array
 def open(char *path):
-	cdef np.ndarray[np.uint8_t, ndim=3] image = cv2.imread(path)
-	return image
+	cdef np.ndarray[np.uint8_t, ndim=3] img = cv2.imread(path)
+	return img
 
 # Saves the given image(numpy-array) into path
-def save(char *path, np.ndarray[double, ndim=2] image):
+def save(char *path, np.ndarray[np.uint8_t, ndim=3] image):
 	cv2.imwrite(path, image)
 
-# Tries to localize the Sudoku in the image
-def cornerDetection(image, imagecolor):
-	#dst = cv2.cornerHarris(imfloat,2,3,0.04)
-	#dst = cv2.dilate(dst,None)
-	dst = np.uint8(image)
+# Scales an image by given factor
+def scale(np.ndarray[np.uint8_t, ndim=3] image, double factor):
+	cdef np.ndarray[np.uint8_t, ndim=3] img = cv2.resize(image, (0, 0), fx=factor, fy=factor)
+	return img
 
-	imgray = cv2.cvtColor(imagecolor,cv2.COLOR_BGR2GRAY)
-	thresh = cv2.threshold(imgray,127,255,0)
-
-	
-
-	contours = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-
-	cv2.drawContours(imagecolor,contours,-1,(0,255,0),3)
-
-	cv2.imwrite("./corners.jpg", imagecolor)
-
-# Manually create the transform-matrix to shift the sudoku into a 1000x1000 image
-def transformManual(np.ndarray[np.uint8_t, ndim=3] image, np.ndarray[double, ndim=2] corners):
-	cdef np.ndarray[double, ndim=1] b, N
-	cdef np.ndarray[double, ndim=2] a, M
-	cdef np.ndarray[np.uint8_t, ndim=3] T
-	cdef double x1, y1, x2, y2, x3, y3, x4, y4, x11, y11, x22, y22, x33, y33, x44, y44
-
-	x1 = corners[0][0]
-	y1 = corners[0][1]
-
-	x2 = corners[1][0]
-	y2 = corners[1][1]
-
-	x3 = corners[2][0]
-	y3 = corners[2][1]
-
-	x4 = corners[3][0]
-	y4 = corners[3][1]
-
-	
-	x11 = 1000.0
-	y11 = 1000.0
-
-	x22 = 1000.0
-	y22 = 0.0
-
-	x33 = 0.0
-	y33 = 0.0
-
-	x44 = 0.0
-	y44 = 1000.0
-
-
-	a = np.array([[ x1, y1, 1, 0, 0, 0, -(x1*x11), -(y1*x11)],
-                  [0, 0, 0, x1, y1, 1, -(x1*y11), -(y1*y11)],
-                  [ x2, y2, 1, 0, 0, 0, -(x2*x22), -(y2*x22)],
-                  [0, 0, 0, x2, y2, 1, -(x2*y22), -(y2*y22)],
-                  [ x3, y3, 1, 0, 0, 0, -(x3*x33), -(y3*x33)],
-                  [0, 0, 0, x3, y3, 1, -(x3*y33), -(y3*y33)],
-                  [ x4, y4, 1, 0, 0, 0, -(x4*x44), -(y4*x44)],
-                  [0, 0, 0, x4, y4, 1, -(x4*y44), -(y4*y44)]])
-
-	b = np.array([x11, y11, x22, y22, x33, y33, x44, y44])
-
-	N = linalg.solve(a, b)
-	N = np.append(N, [1.00])
-	M = N.reshape((3,3))
-
-	T = cv2.warpPerspective(image,M,(1000,1000))
-
-	return T
-
-# Same as transformManual, but the transform-matrix is calculated by 
-# getPerspectiveTransform()
-def transform(np.ndarray[np.uint8_t, ndim=3] image, np.ndarray[double, ndim=2] corners):
-	cdef np.ndarray[double, ndim=2] newCorners, M
-	cdef np.ndarray[np.uint8_t, ndim=3] T
-
-	newCorners = np.array([[1000.0,1000.0],[1000.0,0.0],[0.0,0.0],[0.0,1000.0]])
-
-	M = cv2.getPerspectiveTransform(corners,newCorners)
-	T = cv2.warpPerspective(image,M,(1000,1000))
-
-	return T
-
-# Returns a greyscaled (mean) version of the given image
+# Returns a greyscaled version of the given image
 def greyscale(np.ndarray[np.uint8_t, ndim=3] image):
-	cdef np.ndarray[double, ndim=2] grey = np.mean(image, axis=2)
-	return grey
+	cdef np.ndarray[double, ndim=2] img = np.mean(image, axis=2)
+	return img
 
-# Calculates a binary image, uses the sauvola-algorithm and a window of size*size
-def sauvola(np.ndarray[double, ndim=2] image, int size=15):
+# Blurs the image with gaussianBlur
+def blur(np.ndarray[double, ndim=2] image):
+	cdef np.ndarray[double, ndim=2] img = np.double(cv2.GaussianBlur(image, (5, 5), 0))
+	return img
+
+# Calculates a binary from given greyscale image.
+# Uses the sauvola-algorithm and a window of size*size
+def binary(np.ndarray[double, ndim=2] image, int size=15):
 	cdef np.ndarray[double, ndim=2] tmp0, tmp1, tmp2, va, vb, binary
 	cdef int oldSize, y, x
 	cdef double my, i, o, k, t
@@ -146,40 +75,255 @@ def sauvola(np.ndarray[double, ndim=2] image, int size=15):
 
 	return binary
 
-# Calculates a binary image, uses the niblack-algorithm and a window of size*size
-def niblack(np.ndarray[double, ndim=2] image, int size=15):
-	cdef np.ndarray[double, ndim=2] tmp0, tmp1, tmp2, va, vb, binary
-	cdef int oldSize, y, x
-	cdef double my, i, o, t
+# Calculates the intersection of f and g
+def intersection(np.ndarray[double, ndim=1] f, np.ndarray[double, ndim=1] g):
+	cdef double x, n, intersecX, intersecY
+	cdef np.ndarray[double, ndim=1] intersec
 
-	tmp0 = np.cumsum(image, axis=1)
-	va = np.cumsum(tmp0, axis=0)
-	tmp1=image**2
-	tmp2 = np.cumsum(tmp1, axis=1)
-	vb = np.cumsum(tmp2, axis=0)
-	binary = np.zeros((image.shape[0], image.shape[1]))
+	x = g[0] - f[0]
+	n = f[1] - g[1]
+	intersecX = n / x
+	intersecY = g[0] * intersecX + g[1]
 
-	oldSize = size
+	intersec = np.double([intersecX, intersecY])
+	return intersec
 
+# Tries to localize the Sudoku in the image and gives back the corners in an array
+# image - a binary image
+def cornerDetection(np.ndarray[double, ndim=2] binary):
+	cdef np.ndarray[np.uint8_t, ndim=2] __binary, edges, sudokuArea
+	cdef np.ndarray[double, ndim=1] topLine, bottomLine, leftLine, rightLine
+	cdef np.ndarray[double, ndim=2] equations, sudokuEdges, edgeLines
+	cdef np.ndarray[np.int16_t, ndim=1] boxX, boxY
+	cdef tuple bounding
+	cdef int largestIndex, i, xMin, xMax, yMin, yMax, maxLength
+	cdef double A, largestA, topYIntercept, topXIntercept, bottomYIntercept, bottomXIntercept, leftYIntercept, leftXIntercept, rightYIntercept, rightXIntercept, x, y, m, b
+
+	# Convert the image to uint8, canny needs this
+	__binary = np.uint8(binary)
+
+	# The Canny algorithm detects the edges of the given binary image
+	edges = cv2.Canny(__binary, 50, 100, apertureSize=5)
+
+	# We dilate the Edges, so the Sudoku-Border can be recognized better
+	edges = cv2.dilate(edges, cv2.getStructuringElement(cv2.MORPH_CROSS, (3,3)))
+
+	# findContours, we can use them to find the largest Area in the picture
+	# and assume that this is the sudoku
+	contours, hierarchy = cv2.findContours(edges, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+
+	# We assume that the Contour, with the largest Area is the Sudoku
+	# So we loop over all contours and find the largest
+	largestA = 0
+	largestIndex = 0
+
+	i = 0
+	for contour in contours:
+		
+		A = cv2.contourArea(contour)
+
+		if A > largestA:
+			largestA = A
+			largestIndex = i
+		i+=1
+
+	# Take the smallest (possibly rotated) rectangle of the contour
+	bounding = cv2.minAreaRect(contours[largestIndex])
+
+	# And calculate the Coordinates of the corners
+	box = cv2.cv.BoxPoints(bounding)
+
+	# Put X and Y Values in arrays
+	boxX = np.int16([int(box[0][0]), int(box[1][0]), int(box[2][0]), int(box[3][0])])
+	boxY = np.int16([int(box[0][1]), int(box[1][1]), int(box[2][1]), int(box[3][1])])
+
+	# Get the minima and maxima, the +/- 10 is made, so nothing is 'cut away'
+	xMin = boxX.min() - 10
+	xMax = boxX.max() + 10
+	yMin = boxY.min() - 10
+	yMax = boxY.max() + 10
+
+	# Workaround, the minAreaRect can be negative.
+	# If one or more of the Edges are negative, just make them 0
+	# it's not very beautiful, but it works.
+	if xMin < 0:
+		xMin = 0
+	if xMax < 0:
+		xMax = 0
+	if yMin < 0:
+		yMin = 0
+	if yMax < 0:
+		yMax = 0	
+	
+	# Cut the image to the area, we found with minAreRect
+	sudokuArea = edges[yMin:yMax, xMin:xMax]
+
+	# Get the height or width, whatever is bigger
+	maxLength = max((xMax-xMin), (yMax-yMin))
+
+	# Now calculate the HoughLines
+	lines = cv2.HoughLines(sudokuArea, 1, np.pi/180, int(maxLength*0.5))
+
+	# We start with ridiculous high or low values
+	topYIntercept = 100000.0
+	topXIntercept = 0.0
+
+	bottomYIntercept = 0.0
+	bottomXIntercept = 0.0
+
+	leftXIntercept = 100000.0
+	leftYIntercept = 0.0
+
+	rightXIntercept = 0.0
+	rightYIntercept = 0.0
+
+	topLine = np.double([1000.0,1000.0])
+	bottomLine = np.double([-1000.0,-1000.0])
+	leftLine = np.double([1000.0,1000.0])
+	rightLine = np.double([-1000.0,-1000.0])
+
+	# Loop over all lines, and get the most left, most right, most up and most down
+	for rho,theta in lines[0]:
+
+		# Workaround, when the angle is 0 there would be a divide by zero error, so we assume it is 0.001
+		if theta == 0:
+			theta = 0.001
+
+		x = rho/np.cos(theta)
+		y = rho/(np.cos(theta)*np.sin(theta))
+		
+		# We assume that the top and bottom lines are between 50 and 130 degrees
+		# and the left and right are between 150 and 30 degrees
+		if (theta > np.pi*50/180) and (theta < np.pi*130/180):
+			if (rho < topLine[0]):
+				topLine = np.double([rho, theta])
+
+			if (rho > bottomLine[0]):
+				bottomLine = np.double([rho, theta])
+		elif (theta < np.pi*30/180) or (theta > np.pi*150/180):
+			if (x > rightXIntercept):
+				rightLine = np.double([rho, theta])
+				rightXIntercept = x
+			elif (x <= leftXIntercept):
+				leftLine = np.double([rho, theta])
+				leftXIntercept = x
+
+	# Put the lines in an array
+	edgeLines = np.double([topLine, bottomLine, leftLine, rightLine])
+
+	# top, bottom, left, right
+	equations = np.double([[0,0], [0,0], [0,0], [0,0]])
+
+	# Calculate the equation for every line
+	i = 0
+	for rho,theta in edgeLines:
+		m = (-np.cos(theta))/(np.sin(theta))
+		b = rho/np.sin(theta)
+
+		equations[i] = [m, b]
+
+		i+=1
+
+	# bottom - left | top - left | top - right | bottom - right
+	sudokuEdges = np.double([[0,0], [0,0], [0,0], [0,0]])
+
+	# Calculate the four intersects of the equations
+
+	# bottom - left
+	sudokuEdges[0] = intersection(equations[1], equations[2])
+
+	# top - left
+	sudokuEdges[1] = intersection(equations[0], equations[2])
+
+	# top - right
+	sudokuEdges[2] = intersection(equations[0], equations[3])
+
+	# bottom - right
+	sudokuEdges[3] = intersection(equations[1], equations[3])
+
+
+	# calculate back to global coordinates
+	i = 0
+	for edge in sudokuEdges:
+		sudokuEdges[i] = [(edge[0]+xMin), (edge[1]+yMin)]
+		i+=1
+
+	return sudokuEdges
+
+# Creates the transform-matrix to shift the sudoku from the given corners into an new image
+def transform(np.ndarray[np.uint8_t, ndim=3] image, np.ndarray[double, ndim=2] corners):
+	cdef np.ndarray[double, ndim=1] b, N
+	cdef np.ndarray[double, ndim=2] a, M
+	cdef np.ndarray[np.uint8_t, ndim=3] T
+	cdef double x1, y1, x2, y2, x3, y3, x4, y4, x11, y11, x22, y22, x33, y33, x44, y44
+	cdef int height, width
+
+	x1 = corners[0][0]
+	y1 = corners[0][1]
+
+	x2 = corners[1][0]
+	y2 = corners[1][1]
+
+	x3 = corners[2][0]
+	y3 = corners[2][1]
+
+	x4 = corners[3][0]
+	y4 = corners[3][1]
+
+	height = int(corners[0][1] - corners[1][1])
+	width = int(corners[2][0] - corners[1][0])
+
+	x11 = 0.0
+	y11 = height
+
+	x22 = 0.0
+	y22 = 0.0
+
+	x33 = width
+	y33 = 0.0
+
+	x44 = width
+	y44 = height
+
+
+	a = np.array([[ x1, y1, 1, 0, 0, 0, -(x1*x11), -(y1*x11)],
+                  [0, 0, 0, x1, y1, 1, -(x1*y11), -(y1*y11)],
+                  [ x2, y2, 1, 0, 0, 0, -(x2*x22), -(y2*x22)],
+                  [0, 0, 0, x2, y2, 1, -(x2*y22), -(y2*y22)],
+                  [ x3, y3, 1, 0, 0, 0, -(x3*x33), -(y3*x33)],
+                  [0, 0, 0, x3, y3, 1, -(x3*y33), -(y3*y33)],
+                  [ x4, y4, 1, 0, 0, 0, -(x4*x44), -(y4*x44)],
+                  [0, 0, 0, x4, y4, 1, -(x4*y44), -(y4*y44)]])
+
+	b = np.array([x11, y11, x22, y22, x33, y33, x44, y44])
+
+	N = linalg.solve(a, b)
+	N = np.append(N, [1.00])
+	M = N.reshape((3,3))
+
+	T = cv2.warpPerspective(image,M,(width,height))
+
+	return T
+
+# Creates an array which holds any field in the sudoku
+def raster(np.ndarray[np.uint8_t, ndim=3] image):
+	cdef int imgHeight, imgWidth, fieldHeight, fieldWidth
+	cdef np.ndarray[double, ndim=5] raster
+
+	imgHeight = image.shape[0]
+	imgWidth = image.shape[1]
+
+	fieldHeight = int(imgHeight/9)
+	fieldWidth = int(imgWidth/9)
+
+	raster = np.zeros((9,9,fieldHeight,fieldWidth,3))
+	
 	y = 0
-	for row in image:
+	for line in raster:
 		x = 0
-		for pixel in row:
-			if ((y < size) | (x < size)):
-				size = min(x,y)
+		for field in line:
+			raster[y][x] = np.copy(image[(fieldHeight*y):((fieldHeight*y)+fieldHeight), (fieldWidth*x):((fieldWidth*x)+fieldWidth)])
+			x+=1
+		y+=1
 
-			if (size > 0):
-				my = (va[y][x]-va[y-size][x]-va[y][x-size]+va[y-size][x-size])/size**2
-				i = ((vb[y,x]-vb[y-size,x]-vb[y,x-size]+vb[y-size,x-size])/size**2)-(my**2)
-				if (i < 0):
-					i = 0
-				o = np.sqrt(i)
-				t = my - 0.2 * o
-				if image [y][x] >= t:
-					binary[y][x] = 255
-
-			size = oldSize
-			x = x+1
-		y = y+1
-
-	return binary
+	return raster
