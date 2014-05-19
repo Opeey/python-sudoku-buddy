86c481a6452e77f368d5be4e4005f4d518765f40
Patrick Henrici <patrickhenrici@gmx.de>
2014-05-19 20:43:11 +0200

    changed some parameters, for better results

--- sudoku.py
+++ sudoku.py
@@ -28,11 +28,11 @@ image = sFunc.open(imgIn)
 
 yFactor = 1
 xFactor = 1
-if image.shape[1] > 1200:
-	yFactor = 1200.0/image.shape[1]
+if image.shape[1] > 1100:
+	yFactor = 1100.0/image.shape[1]
 	image = sFunc.scale(image, yFactor)
-if image.shape[0] > 1200:
-	xFactor = 1200.0/image.shape[0]
+if image.shape[0] > 1100:
+	xFactor = 1100.0/image.shape[0]
 	image = sFunc.scale(image, xFactor)
 
 grey = sFunc.greyscale(image)
@@ -43,6 +43,9 @@ binary = sFunc.binarize(blurred, 10)
 corners = sFunc.cornerDetection(binary)
 
 pts = os.path.join('./', 'pts', filename+'.pts')
+#ptsFile = os.path.join('./', filename+'.pts')
+
+#ptsFile = open(ptsFile, 'w')
 
 failed = False
 # check for valid corners, if pts file is specified
@@ -57,20 +60,25 @@ if os.path.isfile(pts):
 		x = (x/yFactor)/xFactor
 		y = (y/yFactor)/xFactor
 
-		if abs(fileX-x) > 100:
+#		ptsFile.write(str(int(x)) + ',' + str(int(y)) + '\n')
+
+		if abs(fileX-x) > 50:
 			print "FAILED: " + str(x) + " is not near " + str(fileX)
 			failed = True
 
-		if abs(fileY-y) > 100:
+		if abs(fileY-y) > 50:
 			print "FAILED: " + str(y) + " is not near " + str(fileY)
 			failed = True
+	f.close()
 
 else:
 	print "WARNING: No .pts file found"
 
-if failed:
+if not failed:
 	print "ERROR: One or more corner-points don't match with the trainings-data, aborting..."
 	sys.exit(-1)
+
+#ptsFile.close()
 
 trans = sFunc.transform(image, corners)
 
